45dd781ac1118464a2f04b3ecaf892e26c3896ae
antonioarieiro <sfgfffs@gmail.com>
2023-06-10 18:24:29 -0300

    default cfg

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+export const useMyStore = defineStore("myStore", {
+  state: () => ({
+    // Add your store state here
+  }),
+  getters: {
+    // Add your store getters here
+  },
+  actions: {
+    // Add your store actions here
+  },
+});
